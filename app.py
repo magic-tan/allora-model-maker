f7bb2d6aa42ff1c007ed63154e6f0b7b52301e60
magic-tan <101536806+magic-tan@users.noreply.github.com>
2025-07-30 14:30:03 +0800

    Update requirements.txt and project files

---
+++ app.py
@@ -0,0 +1,21 @@
+name = "谭政康"
+age = 26
+birth_year = 2025 -age
+print(name, "的出生年份是", birth_year)
+
+
+a = 45
+b = 7
+print("和:", a + b)
+print("差:", a - b)
+print("乘积:", a * b)
+print("商:", a / b)
+
+
+number = -69  # 可以改成其他数字
+if number > 0:
+    print(number, "是正数")
+elif number < 0:
+    print(number, "是负数")
+else:
+    print(number, "是零")
